c336a7665463b935c2425e1da8a7021fc3aa2bda
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2023-05-01 21:20:18 +0300

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,101 @@
+// console.log('Hello world')
+
+// let a = 5
+// let s1 = '222'
+// let s2 = "ssss"
+// let s3 = `sdddd`
+//
+// console.log(s1);
+// s1 = 55
+// console.log(s1);
+// // console.log(a)
+//
+// const myConst = 5
+//
+// console.log(myConst);
+//
+// myConst = 777
+
+// let name = 55
+//
+// let check = true
+// let check2 = false
+//
+//
+// console.log(typeof name);
+// console.log(5<8)
+
+
+// let name = 'Max'
+// let surname = 'Popov'
+// let houseNum = 55
+//
+// // console.log(name, surname, houseNum)
+//
+// let user = name + ' ' + surname + ' ' + houseNum
+//
+// let user2 = `My name is ${name}, surname: ${surname} and house number is ${houseNum}`
+// console.log(user2);
+
+// console.log(5 + 3)
+// console.log(5 - 3)
+// console.log(5 * 3)
+// console.log(5 / 3)
+// console.log(14 % 3)
+// console.log(5 ** 6)
+
+
+// console.log(5<3)
+// console.log(5>3)
+// console.log(5>=3)
+// console.log(5<=3)
+// console.log(5==3)
+// console.log(5===3)
+// console.log(5!=3)
+// console.log(5!==3)
+
+// console.log('5' == 5);
+// console.log('5' === 5);
+// console.log(true !==0);
+
+// !==
+
+// console.log(''+150+150)
+//
+// console.log(+'555');
+// console.log(+'555sss');
+// console.log(typeof (+'555sss'))
+// console.log('555'-2);
+
+// console.log(!!1727);
+// console.log(!!' ');
+// console.log(!!false);
+
+// let check = true
+// let num =0
+// // console.log(!!check);
+// // console.log(!!0)
+//
+// let asd = null
+//
+// console.log(asd);
+
+
+// let a = 5
+// let b =10
+//
+// console.log(a < b);
+
+// if (a<b){
+//     console.log('a<b')
+// }else {
+//     console.log('a>b')
+// }
+
+// console.log('5' === 5); !==
+console.log('555s' * 2);
+// ===
+console.log(5/0)
+// let name = prompt('Enter your name:')
+//
+// console.log(name);
